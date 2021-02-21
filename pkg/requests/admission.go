79d41c6c3a971001d7f51461789e92c0a1208335
Jake Oliver <github@skos.ninja>
2021-02-21 06:44:42 +0000

    Fix admission response Fix logging level on response

--- pkg/requests/admission.go
+++ pkg/requests/admission.go
@@ -48,7 +48,12 @@ func handler(fn admitFunc) http.HandlerFunc {
 		}
 
 		req := v1.AdmissionReview{}
-		resp := v1.AdmissionReview{}
+		resp := v1.AdmissionReview{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "AdmissionReview",
+				APIVersion: "admission.k8s.io/v1",
+			},
+		}
 		if _, _, err := codecs.UniversalDeserializer().Decode(body, nil, &req); err != nil {
 			klog.Error(err)
 			resp.Response = ErrorToAdmissionResponse(err)
@@ -59,14 +64,17 @@ func handler(fn admitFunc) http.HandlerFunc {
 		// The UID have to match
 		resp.Response.UID = req.Request.UID
 
-		klog.Info("response:", resp.Response)
+		var level klog.Level = 0
+		if !resp.Response.Allowed {
+			level = 2
+		}
+		klog.V(level).Info("Validation response: ", resp.Response)
 
 		respBytes, err := json.Marshal(resp)
 		if err != nil {
 			klog.Error(err)
 		}
 
-		w.Header().Set("Content-Type", "application/json")
 		if _, err := w.Write(respBytes); err != nil {
 			klog.Error(err)
 		}
